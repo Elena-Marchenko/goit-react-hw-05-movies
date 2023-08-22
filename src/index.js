0a47a8f51cb82fc430d14ff4a7be08e305927d82
Elena-Marchenko <73835886+Elena-Marchenko@users.noreply.github.com>
2023-08-22 20:45:59 +0300

    reactRouter

--- src/index.js
+++ src/index.js
@@ -5,11 +5,9 @@ import { App } from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/Elena-Marchenko/goit-react-hw-05-movies">
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter basename="/Elena-Marchenko/goit-react-hw-05-movies">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
-
-// {/* <React.StrictMode>
-//     <App />
-//   </React.StrictMode> */}
